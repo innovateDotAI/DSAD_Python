deb58aec89d9e63c503ba0dd44db8bfd70b73c25
innovateDotAI <innovateDotAI@gmail.com>
2024-12-11 14:58:34 +0530

    update string

--- getString.py
+++ getString.py
@@ -1,16 +1,33 @@
 S = '123456789'
 le = 2
-def getString(S,le):
+def getString(S,le,ss):
     if len(S)> le:
         s1 = S[-le:]
         print(s1)
+        ss.append(s1)
         S = S[:-le]
-        getString(S,le)
+        getString(S,le,ss)
     else:
         print(S)
+        ss.append(S)
+def sumString(ss,sl):
+    for t in range(len(ss)):
+        sum=0
+        try:
+            sum = int(ss[t+1]) + int(ss[t+2])
+            tup = (sum,int(ss[t]))
+            sl.append(tup)
+        except:
+            pass
+        
 if __name__=='__main__':
     S = '123456789'
-    le = 4
-    getString(S,le)
+    le = 2
+    ss=[]
+    getString(S,le,ss)
+    print(ss)
+    sl=[]
+    sumString(ss,sl)
+    print(sl)
     
     
